cb0bc8c9126a57f0da908c28f7ff864ae55c2968
Brian Robinson <brob92993@gmail.com>
2021-09-23 21:23:10 -0400

    updated profile page template

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,23 +1,47 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
+import { ListItem, List  } from "../components/List";
+import {Form, TextArea } from "../components/Form"
+
 
 function Profile() {
   return (
     <Container fluid>
-      <Row>
-        <Col size="md-12">
-          <Jumbotron>
-            <h1>404 Page Not Found</h1>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span>
-            </h1>
-          </Jumbotron>
-        </Col>
-      </Row>
-    </Container>
+    <Row>
+      <Col size="md-12">
+       
+        <h1 className= "text-center">Welcome to my profile</h1>
+        <span></span>
+    <div className="text-center">
+      <img alt= "headshot" src="img/avatar-icon.png"/>
+    </div>
+    <Container>  
+        <h4 className= "text-center">Name</h4>
+        <div></div>
+        <p className= "text-center">Billy Bob Joe</p>
+        <div></div>  
+        <h4 className= "text-center">About</h4>
+        <p className= "text-center">About me about me about me about me about me about me about me about me about me about me about me about me about me about me about me about me about me about me about me</p> 
+        </Container> 
+        <h4 className= "text-center">Contact</h4> 
+        <List>
+       <ListItem>
+       <a className="list-group-item list-group-item-action"  href="">1234@1234.com</a>
+       </ListItem>
+       <ListItem>
+       <a className="list-group-item list-group-item-action" href="">Phone: xxx.xxx.xxxx</a>
+       </ListItem>
+          </List> 
+      </Col>
+      <Col size="md-6 sm-12">
+
+            
+
+
+      </Col>
+    </Row>
+  </Container>
   );
 }
 
