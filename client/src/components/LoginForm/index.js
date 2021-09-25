102e190aa4cba75f10a0aea52dd65ea5a3e576da
onava <enava23@yahoo.com>
2021-09-25 10:19:29 -0400

    login/signup components

---
+++ client/src/components/LoginForm/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import './style.css';
+
+// Bulma Stylesheet
+// When making changes to style.. make changes to both css sheets as 
+// both(login/signup) css upload to page and affect both cards since classnames are the same
+
+function LoginForm() {
+  return (
+    <div className="container container-fluid">
+      <div class="box" id="login-up-box">
+      <label class="form-title is-size-5">Login</label>
+        <form class="field control form login-form">  
+          <br></br>
+        <div class="control has-icons-left">
+          <input for="email-login" class="input" type="text" id="email-login" placeholder="Email"></input>
+          <div class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </div>
+        </div>
+          <br></br>
+        <div class="control has-icons-left">
+          <input for="password-login" class="input" type="password" id="password-login" placeholder="Password"></input>
+          <div class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </div>
+        </div>
+        <br></br>
+        <button class="button is-normal is-primary is-fullwidth" type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+    
+  );
+}
+
+export default LoginForm;
+
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,6 +4,9 @@ import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 
+import LoginForm from "../components/LoginForm";
+import SignUpForm from "../components/SignUp";
+
 function Login(props) {
   const [gig, setGig] = useState({})
 
@@ -21,24 +24,9 @@ function Login(props) {
       <Container fluid>
         <Row>
           <Col size="md-12">
-            <Jumbotron>
-            <form>
-              <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-              </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-              </div>
-              <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            </Jumbotron>
+            {/* <Jumbotron /> */}
+            <LoginForm />
+            <SignUpForm />
           </Col>
         </Row>
         <Row>
