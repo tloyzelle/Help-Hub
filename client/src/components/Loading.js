f77814f2bdaf8f94904a0793a1f57a723eccc3c2
Brian Robinson <brob92993@gmail.com>
2021-10-06 18:21:09 -0400

    centering of the loading spinner and added the word loading

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -3,9 +3,18 @@ const loadingImg =
   "https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg";
 
 const Loading = () => (
+  <div className="d-flex justify-content-center">
   <div className="spinner">
     <img src={loadingImg} alt="Loading..." />
+  <div>
+  <p className= "text-center" >
+    <strong> Loading... </strong>
+  </p>
   </div>
+  </div>
+  </div>
+
+  
 );
 
 export default Loading;
